01efae73e696aee0a2230c7bdee68c428aa4c6ad
maulsganteng <muhammadfikri6910@smk.belajar.id>
2023-06-12 15:54:45 +0700

    commit

---
+++ app/Http/Livewire/Pages/Project/CreateComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Project;
+
+use Livewire\Component;
+
+class CreateComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.employe.create-component')->layout("template.app");
+    }
+}
---
+++ app/Http/Livewire/Pages/Project/UpdateComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Project;
+
+use Livewire\Component;
+
+class UpdateComponent extends Component
+{
+   
+    public function render()
+    {
+        return view('livewire.pages.employe.update-component')->layout("template.app");
+    }
+}
